0437ffa3c86b49b5bf9fc51df296d99af1270168
André Grillo <andregrillo@gmail.com>
2023-05-24 18:47:07 +0100

    Empty Message crash when cancel fixed

--- src/ios/AppDelegate+BenefitPay.m
+++ src/ios/AppDelegate+BenefitPay.m
@@ -40,18 +40,70 @@
             statusString = @"failed";
             break;
         default:
+            statusString = @"unknown";
             break;
     }
     
+    NSString* merchantName = [[NSString alloc] init];
+    NSString* cardNumber = [[NSString alloc] init];
+    NSString* currency = [[NSString alloc] init];
+    NSString* currencyCode = [[NSString alloc] init];
+    NSString* amount = [[NSString alloc] init];
+    NSString* message = [[NSString alloc] init];
+    NSString* referenceId = [[NSString alloc] init];
+    
+    if (self.paymentCallback.merchantName == nil || [self.paymentCallback.merchantName length] == 0) {
+        merchantName = @"";
+    } else {
+        merchantName = self.paymentCallback.merchantName;
+    }
+    
+    if (self.paymentCallback.cardNumber == nil || [self.paymentCallback.cardNumber length] == 0) {
+        cardNumber = @"";
+    } else {
+        cardNumber = self.paymentCallback.cardNumber;
+    }
+    
+    if (self.paymentCallback.currency == nil || [self.paymentCallback.currency length] == 0) {
+        currency = @"";
+    } else {
+        currency = self.paymentCallback.currency;
+    }
+        
+    if (self.paymentCallback.currencyCode == nil || [self.paymentCallback.currencyCode length] == 0) {
+        currencyCode = @"";
+    } else {
+        currencyCode = self.paymentCallback.currencyCode;
+    }
+    
+    if (self.paymentCallback.amount == nil || [self.paymentCallback.amount length] == 0) {
+        amount = @"";
+    } else {
+        amount = self.paymentCallback.amount;
+    }
+    
+    if (self.paymentCallback.message == nil || [self.paymentCallback.message length] == 0) {
+        message = @"";
+    } else {
+        message = self.paymentCallback.message;
+    }
+    
+    if (self.paymentCallback.referenceId == nil || [self.paymentCallback.referenceId length] == 0) {
+        referenceId = @"";
+    } else {
+        referenceId = self.paymentCallback.referenceId;
+    }
+    
+    
     NSDictionary *userInfo = @{
         @"Status": statusString,
-        @"MerchantName": self.paymentCallback.merchantName,
-        @"CardNumber": self.paymentCallback.cardNumber,
-        @"Currency": self.paymentCallback.currencyCode,
-        @"CurrencyCode": self.paymentCallback.currencyCode,
-        @"Amount": self.paymentCallback.amount,
-        @"Message": self.paymentCallback.message,
-        @"ReferenceId": self.paymentCallback.referenceId
+        @"MerchantName": merchantName,
+        @"CardNumber": cardNumber,
+        @"Currency": currency,
+        @"CurrencyCode": currencyCode,
+        @"Amount": amount,
+        @"Message": message,
+        @"ReferenceId": referenceId
     };
 
     [[NSNotificationCenter defaultCenter] postNotificationName:kCallbackNotification object:nil userInfo:userInfo];
