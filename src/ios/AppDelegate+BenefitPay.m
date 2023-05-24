8af720df7fb818158bf7709c81638c3fc41b5404
André Grillo <andregrillo@gmail.com>
2023-05-24 19:09:52 +0100

    code cleaning

--- src/ios/AppDelegate+BenefitPay.m
+++ src/ios/AppDelegate+BenefitPay.m
@@ -9,7 +9,6 @@
 
 @implementation AppDelegate (BenefitPay)
 
-
 - (BPDLPaymentCallBackItem *)paymentCallback {
     return objc_getAssociatedObject(self, @selector(paymentCallback));
 }
@@ -44,56 +43,13 @@
             break;
     }
     
-    NSString* merchantName = [[NSString alloc] init];
-    NSString* cardNumber = [[NSString alloc] init];
-    NSString* currency = [[NSString alloc] init];
-    NSString* currencyCode = [[NSString alloc] init];
-    NSString* amount = [[NSString alloc] init];
-    NSString* message = [[NSString alloc] init];
-    NSString* referenceId = [[NSString alloc] init];
-    
-    if (self.paymentCallback.merchantName == nil || [self.paymentCallback.merchantName length] == 0) {
-        merchantName = @"";
-    } else {
-        merchantName = self.paymentCallback.merchantName;
-    }
-    
-    if (self.paymentCallback.cardNumber == nil || [self.paymentCallback.cardNumber length] == 0) {
-        cardNumber = @"";
-    } else {
-        cardNumber = self.paymentCallback.cardNumber;
-    }
-    
-    if (self.paymentCallback.currency == nil || [self.paymentCallback.currency length] == 0) {
-        currency = @"";
-    } else {
-        currency = self.paymentCallback.currency;
-    }
-        
-    if (self.paymentCallback.currencyCode == nil || [self.paymentCallback.currencyCode length] == 0) {
-        currencyCode = @"";
-    } else {
-        currencyCode = self.paymentCallback.currencyCode;
-    }
-    
-    if (self.paymentCallback.amount == nil || [self.paymentCallback.amount length] == 0) {
-        amount = @"";
-    } else {
-        amount = self.paymentCallback.amount;
-    }
-    
-    if (self.paymentCallback.message == nil || [self.paymentCallback.message length] == 0) {
-        message = @"";
-    } else {
-        message = self.paymentCallback.message;
-    }
-    
-    if (self.paymentCallback.referenceId == nil || [self.paymentCallback.referenceId length] == 0) {
-        referenceId = @"";
-    } else {
-        referenceId = self.paymentCallback.referenceId;
-    }
-    
+    NSString *merchantName = (self.paymentCallback.merchantName == nil || self.paymentCallback.merchantName.length == 0) ? @"" : self.paymentCallback.merchantName;
+    NSString *cardNumber = (self.paymentCallback.cardNumber == nil || self.paymentCallback.cardNumber.length == 0) ? @"" : self.paymentCallback.cardNumber;
+    NSString *currency = (self.paymentCallback.currency == nil || self.paymentCallback.currency.length == 0) ? @"" : self.paymentCallback.currency;
+    NSString *currencyCode = (self.paymentCallback.currencyCode == nil || self.paymentCallback.currencyCode.length == 0) ? @"" : self.paymentCallback.currencyCode;
+    NSString *amount = (self.paymentCallback.amount == nil || self.paymentCallback.amount.length == 0) ? @"" : self.paymentCallback.amount;
+    NSString *message = (self.paymentCallback.message == nil || self.paymentCallback.message.length == 0) ? @"" : self.paymentCallback.message;
+    NSString *referenceId = (self.paymentCallback.referenceId == nil || self.paymentCallback.referenceId.length == 0) ? @"" : self.paymentCallback.referenceId;
     
     NSDictionary *userInfo = @{
         @"Status": statusString,
