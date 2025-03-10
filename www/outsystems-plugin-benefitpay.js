814145e50ce5cdaae3b3bce28ba6e64431d42bd7
harikrir <hkrishnan254@gmail.com>
2025-03-10 12:54:24 +0300

    Update outsystems-plugin-benefitpay.js

--- www/outsystems-plugin-benefitpay.js
+++ www/outsystems-plugin-benefitpay.js
@@ -1,13 +1,5 @@
 var exec = require('cordova/exec');
 
-
-
-
-var BenefitPay = {
-    checkout: function (success, error, arg0) {
-        exec(success, error, 'BenefitPay', 'checkout', arg0);
-    }
-}
-
-
-    module.exports = BenefitPay;
+exports.checkout = function (success, error, arg0) {
+    exec(success, error, 'BenefitPay', 'checkout', arg0);
+};
