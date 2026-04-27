ec6088414767be1eb497268e3bd067bcbfaafb48
harikrir <hkrishnan254@gmail.com>
2026-04-27 14:25:29 +0300

    Update AppDelegate+BenefitPay.h

--- src/ios/AppDelegate+BenefitPay.h
+++ src/ios/AppDelegate+BenefitPay.h
@@ -1,8 +1,17 @@
+//
+//  AppDelegate+BenefitPay.h
+//
+//  Created by Andre Grillo on 23/05/2023.
+//
+
 #import "AppDelegate.h"
 #import <BenefitInAppSDK/BenefitInAppSDK.h>
+#import "Constants.h"
 
 @interface AppDelegate (BenefitPay)
 
 @property (nonatomic, strong) BPDLPaymentCallBackItem *paymentCallback;
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options;
 
 @end
