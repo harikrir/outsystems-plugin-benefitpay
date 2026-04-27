9d1cb2de0df9b560a012b9fda589fffe070fbe2b
harikrir <hkrishnan254@gmail.com>
2026-04-27 19:07:22 +0300

    Update AppDelegate+BenefitPay.m

--- src/ios/AppDelegate+BenefitPay.m
+++ src/ios/AppDelegate+BenefitPay.m
@@ -1,15 +1,13 @@
+//
+//  AppDelegate+BenefitPay.m
+//
+//  Created by Andre Grillo on 23/05/2023.
+//
+
 #import "AppDelegate+BenefitPay.h"
 #import <objc/runtime.h>
-#import <os/log.h> // Required for modern logging
 
 @implementation AppDelegate (BenefitPay)
-
-// Define a log category for easy filtering in Console.app
-static os_log_t benefit_log;
-
-+ (void)load {
-    benefit_log = os_log_create("com.aub.benefitpay", "AppDelegate");
-}
 
 - (BPDLPaymentCallBackItem *)paymentCallback {
     return objc_getAssociatedObject(self, @selector(paymentCallback));
@@ -19,54 +17,54 @@ static os_log_t benefit_log;
     objc_setAssociatedObject(self, @selector(paymentCallback), paymentCallback, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-    // Public logging ensures the URL is not redacted in the console
-    os_log(benefit_log, "Deep link received: %{public}@", url.absoluteString);
-    
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options{
     self.paymentCallback = [[BPDLPaymentCallBackItem alloc] initWithDeepLinkURL:url];
     
-    if (!self.paymentCallback) {
-        os_log_error(benefit_log, "Error: Failed to initialize callback item from URL");
-        return NO;
-    }
-
-    NSString* statusString = @"unknown";
+    NSString* statusString = [[NSString alloc] init];
     PaymentCallBackStatus status = self.paymentCallback.status;
-    
     switch (status) {
         case PaymentCallBackStatusCancel:
-            os_log(benefit_log, "Status: Cancelled");
+            // Handle cancel status
+            NSLog(@"Payment status is Cancel");
             statusString = @"cancelled";
             break;
         case PaymentCallBackStatusSuccess:
-            os_log(benefit_log, "Status: Success");
+            // Handle success status
+            NSLog(@"Payment status is Success");
             statusString = @"success";
             break;
         case PaymentCallBackStatusFail:
-            os_log_error(benefit_log, "Status: Failed");
+            // Handle fail status
+            NSLog(@"Payment status is Fail");
             statusString = @"failed";
             break;
         default:
-            os_log_error(benefit_log, "Status: Unknown (%ld)", (long)status);
+            statusString = @"unknown";
             break;
     }
     
-    #define SafeString(str) (str == nil ? @"" : str)
+    NSString *merchantName = (self.paymentCallback.merchantName == nil || self.paymentCallback.merchantName.length == 0) ? @"" : self.paymentCallback.merchantName;
+    NSString *cardNumber = (self.paymentCallback.cardNumber == nil || self.paymentCallback.cardNumber.length == 0) ? @"" : self.paymentCallback.cardNumber;
+    NSString *currency = (self.paymentCallback.currency == nil || self.paymentCallback.currency.length == 0) ? @"" : self.paymentCallback.currency;
+    NSString *currencyCode = (self.paymentCallback.currencyCode == nil || self.paymentCallback.currencyCode.length == 0) ? @"" : self.paymentCallback.currencyCode;
+    NSString *amount = (self.paymentCallback.amount == nil || self.paymentCallback.amount.length == 0) ? @"" : self.paymentCallback.amount;
+    NSString *message = (self.paymentCallback.message == nil || self.paymentCallback.message.length == 0) ? @"" : self.paymentCallback.message;
+    NSString *referenceId = (self.paymentCallback.referenceId == nil || self.paymentCallback.referenceId.length == 0) ? @"" : self.paymentCallback.referenceId;
     
     NSDictionary *userInfo = @{
         @"status": statusString,
-        @"merchantName": SafeString(self.paymentCallback.merchantName),
-        @"cardNumber": SafeString(self.paymentCallback.cardNumber),
-        @"currency": SafeString(self.paymentCallback.currency),
-        @"currencyCode": SafeString(self.paymentCallback.currencyCode),
-        @"amount": SafeString(self.paymentCallback.amount),
-        @"message": SafeString(self.paymentCallback.message),
-        @"referenceId": SafeString(self.paymentCallback.referenceId)
+        @"merchantName": merchantName,
+        @"cardNumber": cardNumber,
+        @"currency": currency,
+        @"currencyCode": currencyCode,
+        @"amount": amount,
+        @"message": message,
+        @"referenceId": referenceId
     };
 
-    os_log(benefit_log, "Posting internal notification: %{public}@", kCallbackNotification);
     [[NSNotificationCenter defaultCenter] postNotificationName:kCallbackNotification object:nil userInfo:userInfo];
     
     return YES;
 }
+
 @end
