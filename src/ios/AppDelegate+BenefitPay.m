c7b9078aa01d084e2efb3f01056f545e52d60c0d
harikrir <hkrishnan254@gmail.com>
2026-04-27 14:25:50 +0300

    Update AppDelegate+BenefitPay.m

--- src/ios/AppDelegate+BenefitPay.m
+++ src/ios/AppDelegate+BenefitPay.m
@@ -1,5 +1,10 @@
+//
+//  AppDelegate+BenefitPay.m
+//
+//  Created by Andre Grillo on 23/05/2023.
+//
+
 #import "AppDelegate+BenefitPay.h"
-#import "Constants.h"
 #import <objc/runtime.h>
 
 @implementation AppDelegate (BenefitPay)
@@ -9,59 +14,56 @@
 }
 
 - (void)setPaymentCallback:(BPDLPaymentCallBackItem *)paymentCallback {
-    objc_setAssociatedObject(
-        self,
-        @selector(paymentCallback),
-        paymentCallback,
-        OBJC_ASSOCIATION_RETAIN_NONATOMIC
-    );
+    objc_setAssociatedObject(self, @selector(paymentCallback), paymentCallback, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
-// ✅ Deep‑link callback from BenefitPay app
-- (BOOL)application:(UIApplication *)app
-            openURL:(NSURL *)url
-            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
-{
-    NSLog(@"✅ BenefitPay openURL: %@", url.absoluteString);
-
-    self.paymentCallback =
-        [[BPDLPaymentCallBackItem alloc] initWithDeepLinkURL:url];
-
-    if (!self.paymentCallback) {
-        return NO;
-    }
-
-    NSString *status = @"failed";
-
-    switch (self.paymentCallback.status) {
-        case PaymentCallBackStatusSuccess:
-            status = @"success";
-            break;
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options{
+    self.paymentCallback = [[BPDLPaymentCallBackItem alloc] initWithDeepLinkURL:url];
+    
+    NSString* statusString = [[NSString alloc] init];
+    PaymentCallBackStatus status = self.paymentCallback.status;
+    switch (status) {
         case PaymentCallBackStatusCancel:
-            status = @"cancelled";
+            // Handle cancel status
+            NSLog(@"Payment status is Cancel");
+            statusString = @"cancelled";
+            break;
+        case PaymentCallBackStatusSuccess:
+            // Handle success status
+            NSLog(@"Payment status is Success");
+            statusString = @"success";
+            break;
+        case PaymentCallBackStatusFail:
+            // Handle fail status
+            NSLog(@"Payment status is Fail");
+            statusString = @"failed";
             break;
         default:
-            status = @"failed";
+            statusString = @"unknown";
             break;
     }
-
+    
+    NSString *merchantName = (self.paymentCallback.merchantName == nil || self.paymentCallback.merchantName.length == 0) ? @"" : self.paymentCallback.merchantName;
+    NSString *cardNumber = (self.paymentCallback.cardNumber == nil || self.paymentCallback.cardNumber.length == 0) ? @"" : self.paymentCallback.cardNumber;
+    NSString *currency = (self.paymentCallback.currency == nil || self.paymentCallback.currency.length == 0) ? @"" : self.paymentCallback.currency;
+    NSString *currencyCode = (self.paymentCallback.currencyCode == nil || self.paymentCallback.currencyCode.length == 0) ? @"" : self.paymentCallback.currencyCode;
+    NSString *amount = (self.paymentCallback.amount == nil || self.paymentCallback.amount.length == 0) ? @"" : self.paymentCallback.amount;
+    NSString *message = (self.paymentCallback.message == nil || self.paymentCallback.message.length == 0) ? @"" : self.paymentCallback.message;
+    NSString *referenceId = (self.paymentCallback.referenceId == nil || self.paymentCallback.referenceId.length == 0) ? @"" : self.paymentCallback.referenceId;
+    
     NSDictionary *userInfo = @{
-        @"status": status,
-        @"merchantName": self.paymentCallback.merchantName ?: @"",
-        @"cardNumber": self.paymentCallback.cardNumber ?: @"",
-        @"currency": self.paymentCallback.currency ?: @"",
-        @"currencyCode": self.paymentCallback.currencyCode ?: @"",
-        @"amount": self.paymentCallback.amount ?: @"",
-        @"message": self.paymentCallback.message ?: @"",
-        @"referenceId": self.paymentCallback.referenceId ?: @""
+        @"status": statusString,
+        @"merchantName": merchantName,
+        @"cardNumber": cardNumber,
+        @"currency": currency,
+        @"currencyCode": currencyCode,
+        @"amount": amount,
+        @"message": message,
+        @"referenceId": referenceId
     };
 
-    // ✅ Post notification for Swift plugin
-    [[NSNotificationCenter defaultCenter]
-        postNotificationName:kCallbackNotification
-                      object:nil
-                    userInfo:userInfo];
-
+    [[NSNotificationCenter defaultCenter] postNotificationName:kCallbackNotification object:nil userInfo:userInfo];
+    
     return YES;
 }
 
