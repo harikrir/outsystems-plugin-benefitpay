45d97ae60de208f47f7c55a3534366df0846d919
harikrir <hkrishnan254@gmail.com>
2026-04-27 15:08:46 +0300

    Create BridgingHeader.h

---
+++ src/ios/BridgingHeader.h
@@ -0,0 +1,12 @@
+//
+//  BridgingHeader.h
+//  This allows your Swift plugin to see the Objective-C AppDelegate and Constants
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import "Constants.h"
+#import "AppDelegate+BenefitPay.h"
+
+#endif /* BridgingHeader_h */
