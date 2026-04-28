ce10ee85b733a0897e60fe53093dfd57f5c5c0b4
harikrir <hkrishnan254@gmail.com>
2026-04-28 09:35:10 +0300

    Create SceneDelegate.m

---
+++ src/ios/SceneDelegate.m
@@ -0,0 +1,20 @@
+#import "SceneDelegate.h"
+#import "AppDelegate.h"
+
+@implementation SceneDelegate
+
+- (void)scene:(UIScene *)scene
+openURLContexts:(NSSet<UIOpenURLContext *> *)URLContexts
+{
+    NSURL *url = URLContexts.anyObject.URL;
+    if (!url) return;
+
+    AppDelegate *delegate =
+      (AppDelegate *)UIApplication.sharedApplication.delegate;
+
+    [delegate application:UIApplication.sharedApplication
+                  openURL:url
+                  options:@{}];
+}
+
+@end
