669de3b7ec851fe2287f7b42aa14a7112b85d332
harikrir <hkrishnan254@gmail.com>
2026-04-28 09:34:46 +0300

    Update AppDelegate+BenefitPay.m

--- src/ios/AppDelegate+BenefitPay.m
+++ src/ios/AppDelegate+BenefitPay.m
@@ -1,73 +1,42 @@
 #import "AppDelegate+BenefitPay.h"
-#import "Constants.h"
 #import <BenefitInAppSDK/BenefitInAppSDK.h>
-#import <objc/runtime.h>
+
+NSString * const BenefitPayCallbackNotification =
+    @"BenefitPayCallbackNotification";
 
 @implementation AppDelegate (BenefitPay)
-
-#pragma mark - Associated Object
-
-- (BPDLPaymentCallBackItem *)paymentCallback {
-    return objc_getAssociatedObject(self, @selector(paymentCallback));
-}
-
-- (void)setPaymentCallback:(BPDLPaymentCallBackItem *)paymentCallback {
-    objc_setAssociatedObject(
-        self,
-        @selector(paymentCallback),
-        paymentCallback,
-        OBJC_ASSOCIATION_RETAIN_NONATOMIC
-    );
-}
-
-#pragma mark - Deep Link Callback
 
 - (BOOL)application:(UIApplication *)application
             openURL:(NSURL *)url
             options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
 {
-    // ✅ REQUIRED by Benefit SDK
     BPDLPaymentCallBackItem *item =
-        [[BPDLPaymentCallBackItem alloc] initWithDeepLinkURL:url];
+      [[BPDLPaymentCallBackItem alloc] initWithDeepLinkURL:url];
 
-    if (!item) {
-        return NO;
-    }
+    if (!item) return NO;
 
-    self.paymentCallback = item;
+    NSString *status = @"failed";
+    if (item.status == PaymentCallBackStatusSuccess) status = @"success";
+    else if (item.status == PaymentCallBackStatusCancel) status = @"cancelled";
 
-    NSString *statusString = @"unknown";
-    switch (item.status) {
-        case PaymentCallBackStatusSuccess:
-            statusString = @"success";
-            break;
-        case PaymentCallBackStatusCancel:
-            statusString = @"cancelled";
-            break;
-        case PaymentCallBackStatusFail:
-            statusString = @"failed";
-            break;
-        default:
-            break;
-    }
-
-    NSDictionary *userInfo = @{
-        @"status"       : statusString,
-        @"merchantName" : item.merchantName ?: @"",
-        @"cardNumber"   : item.cardNumber ?: @"",
-        @"currency"     : item.currency ?: @"",
-        @"currencyCode" : item.currencyCode ?: @"",
-        @"amount"       : item.amount ?: @"",
-        @"message"      : item.message ?: @"",
-        @"referenceId"  : item.referenceId ?: @""
+    NSDictionary *payload = @{
+        @"status": status,
+        @"merchantName": item.merchantName ?: @"",
+        @"cardNumber": item.cardNumber ?: @"",
+        @"currency": item.currency ?: @"",
+        @"currencyCode": item.currencyCode ?: @"",
+        @"amount": item.amount ?: @"",
+        @"message": item.message ?: @"",
+        @"referenceId": item.referenceId ?: @""
     };
 
     [[NSNotificationCenter defaultCenter]
-        postNotificationName:kCallbackNotification
+        postNotificationName:BenefitPayCallbackNotification
                       object:nil
-                    userInfo:userInfo];
+                    userInfo:payload];
 
     return YES;
 }
 
 @end
+``
