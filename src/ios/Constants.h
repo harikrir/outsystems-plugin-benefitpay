ce89453463b417678451897796285c466fd9d4b0
harikrir <hkrishnan254@gmail.com>
2026-04-28 09:27:12 +0300

    Update Constants.h

--- src/ios/Constants.h
+++ src/ios/Constants.h
@@ -1,9 +1,3 @@
-//
-//  Constants.h
-//
-//  Created by Andre Grillo on 22/05/2023.
-//
-
 #import <Foundation/Foundation.h>
 
-extern NSString *const kCallbackNotification;
+extern NSString * const kCallbackNotification;
