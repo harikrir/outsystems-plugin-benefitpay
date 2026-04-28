0c723850f91e2239933f9cf9ad5c2a5fde18871f
harikrir <hkrishnan254@gmail.com>
2026-04-28 13:58:13 +0300

    Update SceneDelegate.m

--- src/ios/SceneDelegate.m
+++ src/ios/SceneDelegate.m
@@ -9,6 +9,8 @@ openURLContexts:(NSSet<UIOpenURLContext *> *)URLContexts
     NSURL *url = URLContexts.anyObject.URL;
     if (!url) return;
 
+    NSLog(@"✅ [BenefitPay] SceneDelegate forwarding URL");
+
     AppDelegate *delegate =
         (AppDelegate *)UIApplication.sharedApplication.delegate;
 
